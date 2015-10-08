5102889a2ee8c3a916d5c1569af3eddd0a1e4ca5
ipatriots <nicolin16@gmail.com>
2015-10-08 09:36:16 -0400

    Google maps modification

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -3,7 +3,7 @@
 function initialize() {
   var mapProp = {
     center:new google.maps.LatLng(48.8567,2.3508),
-    zoom:6,
+    zoom:14,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
