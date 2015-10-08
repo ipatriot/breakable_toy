b6cfff0521a1b7f4218dc004486e4235637642f4
ipatriots <nicolin16@gmail.com>
2015-10-08 10:40:16 -0400

    Google maps modification, now it has markers

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -8,5 +8,19 @@ function initialize() {
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
+
+    var marker = new google.maps.Marker({
+     position: {lat: 48.8567, lng: 2.3508},
+     map: map,
+     title: 'You are here'
+    });
+   marker.addListener('click', function() {
+     infowindow.open(map, marker);
+   });
+
+   var infowindow = new google.maps.InfoWindow({
+     content: 'You are here!'
+   });
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
